434a15e0ce5426e50af822deb9eb85ac7e658c00
Mia OHalloran <miaohall@bu.edu>
2026-04-22 01:16:30 -0400

    styling edits

--- app/page.tsx
+++ app/page.tsx
@@ -4,49 +4,50 @@ import Link from "next/link";
 export default function Home() {
 
     const StyledDiv = styled.div`
-    width: 80vw;
-    margin: 0 auto;
-    background-image: linear-gradient(to bottom, #caf0f8, #90e0ef, #00b4d8);
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    height: 100vh;
-    font-family: monospace;
+        width: 80vw;
+        margin: 0 auto;
+        background-image: linear-gradient(to bottom, #c2f8cb, #219ebc);
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+        font-family: monospace;
 `;
 
     const StyledBox = styled.div`
-    text-align: center;
-    background-color: #0077b6;
-    color: white;
-    font-size: calc(2px + 1.6vw);
-    padding: 10%;
-    border-radius: 20px;
-    border: 5px solid white;
-    margin-left: 10%;
-    margin-right: 10%;
-    box-shadow:0 6px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+        text-align: center;
+        background-color: #023047;
+        color: white;
+        font-size: calc(2px + 1.6vw);
+        padding: 10%;
+        border-radius: 20px;
+        border: 5px dotted white;
+        margin-left: 10%;
+        margin-right: 10%;
+        box-shadow:0 6px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
 `;
 
 
     const StyledButton = styled(Link)`
-    text-decoration: none;
-    color: white;
-    padding: 4% 8%;
-    border-radius: 15px;
-    border: 4px solid white;
-    font-size: calc(2px + 1.7vw);
+        text-decoration: none;
+        padding: 5% 20%;
+        background-color: #bde0fe;
+        color: #023047;
+        border-radius: 15px;
+        font-size: calc(2px + 1.7vw);
     
     &:hover {
         background-color: white;
-        color: #0077b6;
+        color: #023047;
     }
 `;
 
     return (
         <StyledDiv>
             <StyledBox>
-                <h1>OAuth Demo</h1>
+                <h1>OAuth Mini Project</h1>
+                <br></br>
                 <p>Sign in with GitHub below:</p>
                 <br></br><br></br><br></br>
                 <StyledButton href="/api/auth/signin">GitHub</StyledButton>
